1f28ee1c421a5a37fe15b801ff5046738f02f788
Brian Fajardo <brian.fajardo@osedea.com>
2018-07-28 14:28:33 -0400

    Add frontend queries and mutations associated with logging out

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,63 @@
 import React from "react";
+import { graphql } from "react-apollo";
+import { Link } from "react-router";
+
+// Queries
+import GET_AUTHENTICATED_USER from "../queries/GetAuthenticatedUser";
+
+// Mutations
+import LOGOUT from "../mutations/Logout";
 
 class Header extends React.Component {
+  handleLogout = () => {
+    this.props.mutate({
+      refetchQueries: [{ query: GET_AUTHENTICATED_USER }]
+    });
+  };
+
+  renderButtons = () => {
+    const { loading, user } = this.props.data;
+
+    if (loading) {
+      return <div />;
+    }
+
+    if (user) {
+      return (
+        <li>
+          <a onClick={this.handleLogout}>Logout</a>
+        </li>
+      );
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Sign up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
+    }
+  };
+
   render() {
-    return <div>Header</div>;
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default Header;
+export default graphql(LOGOUT)(graphql(GET_AUTHENTICATED_USER)(Header));
--- client/index.js
+++ client/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-client";
+import ApolloClient, { createNetworkInterface } from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 
 import Router from "./Router";
 
+// Creating a custom network interface to be compatible with PassportJS in the backend.
+const networkInterface = createNetworkInterface({
+  uri: "/graphql",
+  opts: {
+    // Send cookies along to PassportJS for queries
+    credentials: "same-origin"
+  }
+});
+
 const client = new ApolloClient({
+  networkInterface,
   // Identify every record from the server so Apollo knows how to store it in local cache.
   dataIdFromObject: o => o.id
 });
